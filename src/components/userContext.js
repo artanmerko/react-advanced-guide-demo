a68f133f261dd29fbae80a927f81651bc77bee89
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-07-22 10:09:21 -0400

    Update userContext.js

--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const UserContext = React.createContext('codevolution')
+const UserContext = React.createContext('codecademy)
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
